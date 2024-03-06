6512910bbb9409b0d10623198e5d9cd3097c751c
nacika-ins <nacika.inscatolare@gmail.com>
2024-03-06 23:10:21 +0900

    fix: グループ管理機能のデザインを修正(Vue3対応)

--- packages/frontend/src/pages/my-groups/group.vue
+++ packages/frontend/src/pages/my-groups/group.vue
@@ -1,178 +1,231 @@
 <template>
-<div class="mk-group-page">
-	<transition :name="$store.state.animation ? 'zoom' : ''" mode="out-in">
-		<div v-if="group" class="_section">
-			<div class="_content" style="display: flex; gap: var(--margin); flex-wrap: wrap;">
-				<MkButton inline @click="invite()">{{ $ts.invite }}</MkButton>
-				<MkButton inline @click="renameGroup()">{{ $ts.rename }}</MkButton>
-				<MkButton inline @click="transfer()">{{ $ts.transfer }}</MkButton>
-				<MkButton inline @click="deleteGroup()">{{ $ts.delete }}</MkButton>
-			</div>
-		</div>
-	</transition>
+  <MkStickyContainer>
+    <template #header><MkPageHeader :actions="headerActions" :tabs="headerTabs" /></template>
+    <div class="mk-group-page">
+      <transition :name="$store.state.animation ? 'zoom' : ''" mode="out-in">
+        <div v-if="group" class="buttons">
+          <div class="_content" style="display: flex; gap: var(--margin); flex-wrap: wrap">
+            <MkButton inline @click="invite()">{{ i18n.ts.invite }}</MkButton>
+            <MkButton inline @click="renameGroup()">{{ i18n.ts.rename }}</MkButton>
+            <MkButton inline @click="transfer()">{{ i18n.ts.transfer }}</MkButton>
+            <MkButton inline @click="deleteGroup()">{{ i18n.ts.delete }}</MkButton>
+          </div>
+        </div>
+      </transition>
 
-	<transition :name="$store.state.animation ? 'zoom' : ''" mode="out-in">
-		<div v-if="group" class="_section members _gap">
-			<div class="_title">{{ $ts.members }}</div>
-			<div class="_content">
-				<div class="users">
-					<div v-for="user in users" :key="user.id" class="user _panel">
-						<MkAvatar :user="user" class="avatar" :show-indicator="true"/>
-						<div class="body">
-							<MkUserName :user="user" class="name"/>
-							<MkAcct :user="user" class="acct"/>
-						</div>
-						<div class="action">
-							<button class="_button" @click="removeUser(user)"><i class="fas fa-times"></i></button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</transition>
-</div>
+      <transition :name="$store.state.animation ? 'zoom' : ''" mode="out-in">
+        <div v-if="group" class="_section members _gap">
+          <div class="_content">
+            <div class="users">
+              <div v-for="user in users" :key="user.id" class="user _panel">
+                <div class="_user_content">
+                  <MkAvatar :user="user" class="avatar" :show-indicator="true" />
+                  <MkA :to="`/users/${user.id}`">
+                    <div class="body">
+                      <MkUserName :user="user" class="name" />
+                      <MkAcct :user="user" class="acct" />
+                    </div>
+                  </MkA>
+                </div>
+                <div class="action">
+                  <button class="_button" @click="removeUser(user)">
+                    <MkButton danger>{{ i18n.ts.remove }}</MkButton>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
+  </MkStickyContainer>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent } from 'vue';
+<script lang="ts" setup>
+import { computed, onActivated, onMounted, watch } from 'vue';
 import MkButton from '@/components/MkButton.vue';
 import * as os from '@/os';
-import * as symbols from '@/symbols';
+import MkA from '@/components/global/MkA.vue';
+import MkAvatar from '@/components/global/MkAvatar.vue';
+import { i18n } from '@/i18n';
+import { definePageMetadata } from '@/scripts/page-metadata';
 
-export default defineComponent({
-	components: {
-		MkButton
-	},
+const props = withDefaults(
+  defineProps<{
+    groupId: {
+      type: string;
+      required: true;
+    };
+  }>(),
+  {},
+);
 
-	props: {
-		groupId: {
-			type: String,
-			required: true,
-		},
-	},
+let group = $ref();
+let users = $ref([]);
+let headerActions = $computed(() => []);
+let headerTabs = $computed(() => []);
+watch(() => props.groupId, fetch);
 
-	data() {
-		return {
-			[symbols.PAGE_INFO]: computed(() => this.group ? {
-				title: this.group.name,
-				icon: 'fas fa-users',
-			} : null),
-			group: null,
-			users: [],
-		};
-	},
+let isFirstFetch = $ref(true);
 
-	watch: {
-		groupId: 'fetch',
-	},
-
-	created() {
-		this.fetch();
-	},
-
-	methods: {
-		fetch() {
-			os.api('users/groups/show', {
-				groupId: this.groupId
-			}).then(group => {
-				this.group = group;
-				os.api('users/show', {
-					userIds: this.group.userIds
-				}).then(users => {
-					this.users = users;
-				});
-			});
-		},
-
-		invite() {
-			os.selectUser().then(user => {
-				os.apiWithDialog('users/groups/invite', {
-					groupId: this.group.id,
-					userId: user.id
-				});
-			});
-		},
-
-		removeUser(user) {
-			os.api('users/groups/pull', {
-				groupId: this.group.id,
-				userId: user.id
-			}).then(() => {
-				this.users = this.users.filter(x => x.id !== user.id);
-			});
-		},
-
-		async renameGroup() {
-			const { canceled, result: name } = await os.inputText({
-				title: this.$ts.groupName,
-				default: this.group.name
-			});
-			if (canceled) return;
-
-			await os.api('users/groups/update', {
-				groupId: this.group.id,
-				name: name
-			});
-
-			this.group.name = name;
-		},
-
-		transfer() {
-			os.selectUser().then(user => {
-				os.apiWithDialog('users/groups/transfer', {
-					groupId: this.group.id,
-					userId: user.id
-				});
-			});
-		},
-
-		async deleteGroup() {
-			const { canceled } = await os.confirm({
-				type: 'warning',
-				text: this.$t('removeAreYouSure', { x: this.group.name }),
-			});
-			if (canceled) return;
-
-			await os.apiWithDialog('users/groups/delete', {
-				groupId: this.group.id
-			});
-			this.$router.push('/my/groups');
-		}
-	}
+onMounted(async () => {
+  await fetch();
 });
+
+onActivated(() => {
+  if (isFirstFetch) return;
+  fetch();
+});
+
+definePageMetadata(
+  computed(() => ({
+    title: group ? group.name : i18n.ts.members,
+    icon: 'ti ti-users',
+  })),
+);
+
+/**
+ * データ取得
+ */
+async function fetch() {
+  os.api('users/groups/show', {
+    groupId: props.groupId,
+  })
+    .then((_group) => {
+      group = _group;
+      os.api('users/show', {
+        userIds: group.userIds,
+      }).then((_users) => {
+        users = _users;
+      });
+    })
+    .finally(() => {
+      isFirstFetch = false;
+    });
+}
+
+/**
+ * 招待
+ */
+async function invite() {
+  os.selectUser().then((user) => {
+    os.apiWithDialog('users/groups/invite', {
+      groupId: group.id,
+      userId: user.id,
+    });
+  });
+}
+
+/**
+ * ユーザー削除
+ */
+async function removeUser(user) {
+  os.api('users/groups/pull', {
+    groupId: group.id,
+    userId: user.id,
+  }).then(() => {
+    users = users.filter((x) => x.id !== user.id);
+  });
+}
+
+/**
+ * グループ名変更
+ */
+async function renameGroup() {
+  const { canceled, result: name } = await os.inputText({
+    title: i18n.groupName,
+    default: group.name,
+  });
+  if (canceled) return;
+
+  await os.api('users/groups/update', {
+    groupId: group.id,
+    name: name,
+  });
+
+  group.name = name;
+}
+
+/**
+ * 譲渡
+ */
+async function transfer() {
+  os.selectUser().then((user) => {
+    os.apiWithDialog('users/groups/transfer', {
+      groupId: group.id,
+      userId: user.id,
+    });
+  });
+}
+
+/**
+ * グループ削除
+ */
+async function deleteGroup() {
+  const { canceled } = await os.confirm({
+    type: 'warning',
+    text: i18n.t('removeAreYouSure', { x: group.name }),
+  });
+  if (canceled) return;
+
+  await os.apiWithDialog('users/groups/delete', {
+    groupId: group.id,
+  });
+  await fetch();
+}
 </script>
 
 <style lang="scss" scoped>
 .mk-group-page {
-	> .members {
-		> ._content {
-			> .users {
-				> .user {
-					display: flex;
-					align-items: center;
-					padding: 16px;
+  > .buttons {
+    display: flex;
+    gap: var(--margin);
+    flex-wrap: wrap;
+    margin: 1em;
+  }
 
-					> .avatar {
-						width: 50px;
-						height: 50px;
-					}
+  > .members {
+    > ._content {
+      > .users {
+        > .user {
+          margin: 1em;
+          padding: 1em;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
 
-					> .body {
-						flex: 1;
-						padding: 8px;
+          ._user_content {
+            display: flex;
+            align-items: center;
+            gap: var(--margin);
 
-						> .name {
-							display: block;
-							font-weight: bold;
-						}
+            > .avatar {
+              width: 50px;
+              height: 50px;
+            }
 
-						> .acct {
-							opacity: 0.5;
-						}
-					}
-				}
-			}
-		}
-	}
+            > .body {
+              flex: 1;
+              padding: 8px;
+
+              > .name {
+                display: block;
+                font-weight: bold;
+              }
+
+              > .acct {
+                opacity: 0.5;
+              }
+            }
+
+            > .actions {
+              display: flex;
+              gap: var(--margin);
+            }
+          }
+        }
+      }
+    }
+  }
 }
 </style>
--- packages/frontend/src/pages/my-groups/index.vue
+++ packages/frontend/src/pages/my-groups/index.vue
@@ -1,148 +1,244 @@
 <template>
-<MkSpacer :content-max="700">
-	<div v-if="tab === 'owned'" class="_content">
-		<MkButton primary style="margin: 0 auto var(--margin) auto;" @click="create"><i class="fas fa-plus"></i> {{ $ts.createGroup }}</MkButton>
+  <MkStickyContainer>
+    <template #header>
+      <MkPageHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs" />
+    </template>
+    <MkSpacer :content-max="700">
+      <div v-if="tab === 'owned'" class="_content">
+        <MkButton primary style="margin: 0 auto var(--margin) auto" @click="create">
+          <i class="fas fa-plus"></i> {{ i18n.ts.createGroup }}
+        </MkButton>
 
-		<MkPagination v-slot="{items}" ref="owned" :pagination="ownedPagination">
-			<div v-for="group in items" :key="group.id" class="_card">
-				<div class="_title"><MkA :to="`/my/groups/${ group.id }`" class="_link">{{ group.name }}</MkA></div>
-				<div class="_content"><MkAvatars :user-ids="group.userIds"/></div>
-			</div>
-		</MkPagination>
-	</div>
+        <MkPagination v-slot="{ items }" ref="owned" :pagination="ownedPagination">
+          <div v-for="group in items" :key="group.id" class="_card">
+            <div class="_title">
+              <div class="_title__name">
+                <MkA :to="`/my/groups/${group.id}`" class="_link">{{ group.name }}</MkA>
+              </div>
+            </div>
+            <div class="_divider" />
+            <div class="_content">
+              <MkAvatars :user-ids="group.userIds" />
+            </div>
+          </div>
+        </MkPagination>
+      </div>
 
-	<div v-else-if="tab === 'joined'" class="_content">
-		<MkPagination v-slot="{items}" ref="joined" :pagination="joinedPagination">
-			<div v-for="group in items" :key="group.id" class="_card">
-				<div class="_title">{{ group.name }}</div>
-				<div class="_content"><MkAvatars :user-ids="group.userIds"/></div>
-				<div class="_footer">
-					<MkButton danger @click="leave(group)">{{ $ts.leaveGroup }}</MkButton>
-				</div>
-			</div>
-		</MkPagination>
-	</div>
+      <div v-else-if="tab === 'joined'" class="_content">
+        <MkPagination v-slot="{ items }" ref="joined" :pagination="joinedPagination">
+          <div v-for="group in items" :key="group.id" class="_card">
+            <div class="_title">
+              <div class="_title__name">{{ group.name }}</div>
+              <div class="_buttons">
+                <MkButton danger @click="leave(group)">{{ i18n.ts.leaveGroup }}</MkButton>
+              </div>
+            </div>
+            <div class="_divider" />
+            <div class="_content">
+              <MkAvatars :user-ids="group.userIds" />
+            </div>
+          </div>
+        </MkPagination>
+      </div>
 
-	<div v-else-if="tab === 'invites'" class="_content">
-		<MkPagination v-slot="{items}" ref="invitations" :pagination="invitationPagination">
-			<div v-for="invitation in items" :key="invitation.id" class="_card">
-				<div class="_title">{{ invitation.group.name }}</div>
-				<div class="_content"><MkAvatars :user-ids="invitation.group.userIds"/></div>
-				<div class="_footer">
-					<MkButton primary inline @click="acceptInvite(invitation)"><i class="fas fa-check"></i> {{ $ts.accept }}</MkButton>
-					<MkButton primary inline @click="rejectInvite(invitation)"><i class="fas fa-ban"></i> {{ $ts.reject }}</MkButton>
-				</div>
-			</div>
-		</MkPagination>
-	</div>
-</MkSpacer>
+      <div v-else-if="tab === 'invites'" class="_content">
+        <MkPagination v-slot="{ items }" ref="invitations" :pagination="invitationPagination">
+          <div v-for="invitation in items" :key="invitation.id" class="_card">
+            <div class="_title">
+              <div class="_title__name">{{ invitation.group.name }}</div>
+              <div class="_buttons">
+                <MkButton primary inline @click="acceptInvite(invitation)">
+                  <i class="fas fa-check"></i> {{ i18n.ts.accept }}
+                </MkButton>
+                <MkButton danger inline @click="rejectInvite(invitation)">
+                  <i class="fas fa-ban"></i> {{ i18n.ts.reject }}
+                </MkButton>
+              </div>
+            </div>
+            <div class="_divider" />
+            <div class="_content">
+              <MkAvatars :user-ids="invitation.group.userIds" />
+            </div>
+          </div>
+        </MkPagination>
+      </div>
+    </MkSpacer>
+  </MkStickyContainer>
 </template>
 
-<script lang="ts">
-import { defineComponent, computed } from 'vue';
+<script lang="ts" setup>
+import { computed, shallowRef } from 'vue';
 import MkPagination from '@/components/MkPagination.vue';
 import MkButton from '@/components/MkButton.vue';
-import MkContainer from '@/components/MkContainer.vue';
-import MkTab from '@/components/MkTab.vue';
-import MkAvatars from '@/components/MkAvatars.vue';
 import * as os from '@/os';
-import * as symbols from '@/symbols';
+import { i18n } from '@/i18n';
+import { definePageMetadata } from '@/scripts/page-metadata';
+import MkAvatars from '@/components/MkAvatars.vue';
 
+const props = withDefaults(
+  defineProps<{
+    initialTab?: string;
+  }>(),
+  {
+    initialTab: 'owned',
+  },
+);
 
-export default defineComponent({
-	components: {
-		MkPagination,
-		MkButton,
-		MkContainer,
-		MkTab,
-		MkAvatars,
-	},
+const headerActions = $computed(() => []);
 
-	data() {
-		return {
-			[symbols.PAGE_INFO]: computed(() => ({
-				title: this.$ts.groups,
-				icon: 'fas fa-users',
-				bg: 'var(--bg)',
-				actions: [{
-					icon: 'fas fa-plus',
-					text: this.$ts.createGroup,
-					handler: this.create,
-				}],
-				tabs: [{
-					active: this.tab === 'owned',
-					title: this.$ts.ownedGroups,
-					icon: 'fas fa-user-tie',
-					onClick: () => { this.tab = 'owned'; },
-				}, {
-					active: this.tab === 'joined',
-					title: this.$ts.joinedGroups,
-					icon: 'fas fa-id-badge',
-					onClick: () => { this.tab = 'joined'; },
-				}, {
-					active: this.tab === 'invites',
-					title: this.$ts.invites,
-					icon: 'fas fa-envelope-open-text',
-					onClick: () => { this.tab = 'invites'; },
-				},]
-			})),
-			tab: 'owned',
-			ownedPagination: {
-				endpoint: 'users/groups/owned' as const,
-				limit: 10,
-			},
-			joinedPagination: {
-				endpoint: 'users/groups/joined' as const,
-				limit: 10,
-			},
-			invitationPagination: {
-				endpoint: 'i/user-group-invites' as const,
-				limit: 10,
-			},
-		};
-	},
+const headerTabs = $computed(() => [
+  {
+    key: 'owned',
+    icon: 'ti ti-bolt',
+    title: i18n.ts.ownedGroups,
+  },
+  {
+    key: 'joined',
+    icon: 'ti ti-users',
+    title: i18n.ts.joinedGroups,
+  },
+  {
+    key: 'invites',
+    title: i18n.ts.invites,
+  },
+]);
 
-	methods: {
-		async create() {
-			const { canceled, result: name } = await os.inputText({
-				title: this.$ts.groupName,
-			});
-			if (canceled) return;
-			await os.api('users/groups/create', { name: name });
-			this.$refs.owned.reload();
-			os.success();
-		},
-		acceptInvite(invitation) {
-			os.api('users/groups/invitations/accept', {
-				invitationId: invitation.id
-			}).then(() => {
-				os.success();
-				this.$refs.invitations.reload();
-				this.$refs.joined.reload();
-			});
-		},
-		rejectInvite(invitation) {
-			os.api('users/groups/invitations/reject', {
-				invitationId: invitation.id
-			}).then(() => {
-				this.$refs.invitations.reload();
-			});
-		},
-		async leave(group) {
-			const { canceled } = await os.confirm({
-				type: 'warning',
-				text: this.$t('leaveGroupConfirm', { name: group.name }),
-			});
-			if (canceled) return;
-			os.apiWithDialog('users/groups/leave', {
-				groupId: group.id,
-			}).then(() => {
-				this.$refs.joined.reload();
-			});
-		}
-	}
+let tab = $ref(props.initialTab);
+
+let invitations = shallowRef();
+let owned = shallowRef();
+let joined = shallowRef();
+
+/**
+ * グループを作成
+ */
+async function create() {
+  const { canceled, result: name } = await os.inputText({
+    title: i18n.ts.groupName,
+  });
+  if (canceled) return;
+  await os.api('users/groups/create', { name: name });
+  owned.value.reload();
+  os.success();
+}
+
+/**
+ * 招待を承認する
+ */
+function acceptInvite(invitation) {
+  os.api('users/groups/invitations/accept', {
+    invitationId: invitation.id,
+  }).then(() => {
+    os.success();
+    invitations.value.reload();
+    joined.value.reload();
+  });
+}
+
+/**
+ * 招待を拒否する
+ */
+function rejectInvite(invitation) {
+  os.api('users/groups/invitations/reject', {
+    invitationId: invitation.id,
+  }).then(() => {
+    invitations.value.reload();
+  });
+}
+
+/**
+ * グループから脱退する
+ */
+async function leave(group) {
+  const { canceled } = await os.confirm({
+    type: 'warning',
+    text: i18n.t('leaveGroupConfirm', { name: group.name }),
+  });
+  if (canceled) return;
+  os.apiWithDialog('users/groups/leave', {
+    groupId: group.id,
+  }).then(() => {
+    joined.value.reload();
+  });
+}
+
+let ownedPagination = $ref({
+  endpoint: 'users/groups/owned' as const,
+  limit: 10,
 });
+let joinedPagination = $ref({
+  endpoint: 'users/groups/joined' as const,
+  limit: 10,
+});
+let invitationPagination = $ref({
+  endpoint: 'i/user-group-invites' as const,
+  limit: 10,
+});
+
+definePageMetadata(
+  computed(() => ({
+    title: i18n.ts.groups,
+    icon: 'ti ti-users',
+    bg: 'var(--bg)',
+    actions: [
+      {
+        icon: 'fas fa-plus',
+        text: i18n.ts.createGroup,
+        handler: create,
+      },
+    ],
+  })),
+);
 </script>
 
 <style lang="scss" scoped>
+._content {
+  ._card {
+    padding: 1em;
+    border: 1px solid var(--border);
+    border-radius: 0.5em;
+    margin-bottom: 1em;
+
+    ._title {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      background: var(--panel);
+      color: var(--fg);
+      font-size: 1.2em;
+      font-weight: bold;
+      padding: 0.25em;
+      gap: 0.25em;
+      border-radius: 0.5em 0.5em 0 0;
+
+      ._title__name {
+        margin: 0.5em;
+      }
+    }
+
+    ._divider {
+      border-bottom: 1px solid var(--divider);
+    }
+
+    ._content {
+      display: flex;
+      gap: 0.5em;
+      margin-bottom: 0.5em;
+      padding: 1em;
+      background: var(--panel);
+      color: var(--fg);
+      border-radius: 0 0 0.5em 0.5em;
+    }
+
+    ._buttons {
+      display: flex;
+      row-gap: 0.5em;
+      justify-content: flex-end;
+      padding: 1em;
+      color: var(--fg);
+      border-radius: 0 0 0.5em 0.5em;
+      font-size: 0.8em;
+    }
+  }
+}
 </style>
