a0e0ebee19a65012028eda0ab73797484f221f7b
nacika-ins <nacika.inscatolare@gmail.com>
2024-06-13 14:08:51 +0900

    fix: 検索機能のコード戻し

--- packages/backend/src/server/api/endpoints/notes/search.ts
+++ packages/backend/src/server/api/endpoints/notes/search.ts
@@ -83,6 +83,24 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 				query.andWhere('note.channelId = :channelId', { channelId: ps.channelId });
 			}
 
+			query
+				.andWhere('note.text ILIKE :q', { q: `%${ sqlLikeEscape(ps.query) }%` })
+				.innerJoinAndSelect('note.user', 'user')
+				.leftJoinAndSelect('user.avatar', 'avatar')
+				.leftJoinAndSelect('user.banner', 'banner')
+				.leftJoinAndSelect('note.reply', 'reply')
+				.leftJoinAndSelect('note.renote', 'renote')
+				.leftJoinAndSelect('reply.user', 'replyUser')
+				.leftJoinAndSelect('replyUser.avatar', 'replyUserAvatar')
+				.leftJoinAndSelect('replyUser.banner', 'replyUserBanner')
+				.leftJoinAndSelect('renote.user', 'renoteUser')
+				.leftJoinAndSelect('renoteUser.avatar', 'renoteUserAvatar')
+				.leftJoinAndSelect('renoteUser.banner', 'renoteUserBanner');
+
+			this.queryService.generateVisibilityQuery(query, me);
+			if (me) this.queryService.generateMutedUserQuery(query, me);
+			if (me) this.queryService.generateBlockedUserQuery(query, me);
+
 			const notes = await query.take(ps.limit).getMany();
 			return await this.noteEntityService.packMany(notes, me);
 		});
