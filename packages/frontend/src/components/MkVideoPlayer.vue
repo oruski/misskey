7ba7e8b5b2ef28587d22b787a5494add7fbd1f6f
nacika-ins <nacika.inscatolare@gmail.com>
2024-09-19 03:04:03 +0900

    fix: 動画再生プレイヤーの差し替え
    (cherry picked from commit 799a3a424f89b196e0fe08141d5162646dd96b2c)

---
+++ packages/frontend/src/components/MkVideoPlayer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <video ref="videoPlayer" class="video-js" vjs-default-skin data-setup='{"fluid": true}'></video>
+  </div>
+</template>
+
+<script>
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+
+export default {
+  name: 'VideoPlayer',
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  data() {
+    return {
+      player: null,
+    };
+  },
+  mounted() {
+    this.player = videojs(this.$refs.videoPlayer, this.options, () => {
+      this.player.log('onPlayerReady', this);
+    });
+  },
+  beforeUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  },
+};
+</script>
+
+<style lang="scss" module>
+.video-js {
+  width: 100% !important;
+  height: 100% !important;
+}
+</style>
